801bc501e879a7e96fc14fe3002da33a22cac281
xuzuxing595 <1078509454@qq.com>
2018-01-09 17:17:36 +0800

    版权更新

--- oobio/common/function.php
+++ oobio/common/function.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Created by 小羊.
+ * Oobio - 简单、高效的PHP微框架
+ * Copyright (c) 2018 Oobio . All rights reserved.
  * Author: 勇敢的小笨羊
- * 微博: http://weibo.com/xuzuxing
- * Date: 2018/1/8
- * Time: 21:52
+ * Github: https://github.com/superpig595/OoBio
+ * Weibo: http://weibo.com/xuzuxing
+ *
  */
 
 
--- oobio/lib/conf.php
+++ oobio/lib/conf.php
@@ -3,8 +3,9 @@
  * Oobio - 简单、高效的PHP微框架
  * Copyright (c) 2018 Oobio . All rights reserved.
  * Author: 勇敢的小笨羊
- * 微博: http://weibo.com/xuzuxing
- * Date: 2018/1/8
+ * Github: https://github.com/superpig595/OoBio
+ * Weibo: http://weibo.com/xuzuxing
+ *
  */
 
 // Oobio 配置类
